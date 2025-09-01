79c3b1baaa53fd13ab042e60864691d83db78ab0
JABYAEM <6631503124@lamduan.mfu.ac.th>
2025-09-01 09:50:22 +0000

    Done05

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -1,6 +1,36 @@
 package th.mfu.boot;
 
-public class User {
+import java.time.LocalDate;
 
-    
+import org.springframework.lang.NonNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+public class User {
+    private String username;
+    private String displayname;
+    private String email;
+
+
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getDisplayname() {
+        return displayname;
+    }
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
